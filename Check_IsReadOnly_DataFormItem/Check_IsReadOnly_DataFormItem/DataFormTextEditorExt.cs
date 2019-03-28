d659695c48e5a42ac83ac95c88606e12e92760a2
Ganeshamoorthy C <ganeshamoorthy.chandramoorthy@syncfusion.com>
2019-03-28 15:25:44 +0530

    Sample committed.

---
+++ Check_IsReadOnly_DataFormItem/Check_IsReadOnly_DataFormItem/DataFormTextEditorExt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+using Syncfusion.Android.DataForm;
+using Syncfusion.Android.DataForm.Editors;
+
+namespace Check_IsReadOnly_DataFormItem
+{
+    public class DataFormTextEditorExt : DataFormTextEditor
+    {
+        public DataFormTextEditorExt(SfDataForm dataForm) : base(dataForm)
+        {
+
+        }
+
+        protected override void OnWireEvents(EditText view)
+        {
+            view.Touch += OnTouch;
+            base.OnWireEvents(view);
+        }
+
+        private void OnTouch(object sender, View.TouchEventArgs e)
+        {
+            var isReadOnly = ((sender as EditText).Parent as DataFormItemView).DataFormItem.IsReadOnly;
+        }
+
+        protected override void OnUnWireEvents(EditText view)
+        {
+            view.Touch -= OnTouch;
+            base.OnUnWireEvents(view);
+        }
+    }
+}
